1f526816e8e94e8766d6c3cef859b561ecc2e7a0
Cocoa <110008465+0arari0@users.noreply.github.com>
2023-11-02 00:10:33 +0900

    대규모 UI 작업
    1. 앨리스 애니메이션 새로 추가 (Alice_character 파일에 있는 앨리스 사용) - down 애니메이션 설정을 잘못했습니다..ㅎㅎ 수정 부탁드려요!!
    
    2. 적군 스프라이트 파일 추가 - 각 캐릭터 별 이름 수정했으니 해당 이름에 맞게 애니메이션 추가 바랍니다.
    
    3. 메인 화면 씬 구현 및 인게임 UI 바 구현 - 인게임 좌측 하단의 라이프 구현 부탁드립니다. (몬스터와 접촉 시 라이프 이미지 1개 차감)

--- AliceProject/Assets/Script/Scene01/SceneManager01.cs
+++ AliceProject/Assets/Script/Scene01/SceneManager01.cs
@@ -1,10 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager01 : SceneManager00
 {
     #region 프로퍼티
     public override string SceneName => Global.SCENE_NAME_01;
     #endregion
+    [SerializeField] public GameObject SettingPanel;
+    [SerializeField] public GameObject ExitPanel;
+
+    //Go to Play Scene
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Active SettingPanel
+    public void SettingPanelOpen()
+    {
+            SettingPanel.gameObject.SetActive(true);
+    }
+
+    //Active ExitPanel
+    public void ExitPanelOpen()
+    {
+        ExitPanel.gameObject.SetActive(true);
+    }
+
+    //Exit Game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
